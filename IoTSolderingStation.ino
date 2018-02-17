aa47d8849ddc5f891b331b8b630343ad8f392344
V. Kutsarit <kutsarit@web.de>
2018-02-17 22:03:26 +0100

    Rename file

--- IoTSolderingStation.ino
+++ IoTSolderingStation.ino
@@ -1,9 +1,14 @@
 
 #include <Wire.h>
-#include <esp8266-oled-ssd1306\SSD1306.h>
+#include <SSD1306.h>
 #include "RotaryEncoder.h"
 //#include <U8g2lib.h>
 #include "esp_attr.h"
+#include <Adafruit_MAX31856.h>
+#include <SPI.h>
+
+// https://github.com/ThingPulse/esp8266-oled-ssd1306
+// https://github.com/adafruit/Adafruit_MAX31856
 
 #define OLED_CLOCK 4
 #define OLED_DATA 5
@@ -13,6 +18,7 @@
 //U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE, OLED_CLOCK, OLED_DATA);
 SSD1306 display2(0x3c, 5, 4);
 RotaryEncoder rotary(ROTARY_A, ROTARY_B, 5, 10, 1000 / 10, 1000 / 5);
+Adafruit_MAX31856 tempReader(2,13,12,14);
 
 unsigned int count = 0;
 
@@ -35,6 +41,24 @@ void setup()
 	attachInterrupt(digitalPinToInterrupt(ROTARY_A), handleInterruptRotary, CHANGE);
 	attachInterrupt(digitalPinToInterrupt(ROTARY_B), handleInterruptRotary, CHANGE);
 	
+	tempReader.begin();
+	tempReader.setThermocoupleType(MAX31856_TCTYPE_K);
+
+	Serial.print("Thermocouple type: ");
+	switch (tempReader.getThermocoupleType()) {
+	case MAX31856_TCTYPE_B: Serial.println("B Type"); break;
+	case MAX31856_TCTYPE_E: Serial.println("E Type"); break;
+	case MAX31856_TCTYPE_J: Serial.println("J Type"); break;
+	case MAX31856_TCTYPE_K: Serial.println("K Type"); break;
+	case MAX31856_TCTYPE_N: Serial.println("N Type"); break;
+	case MAX31856_TCTYPE_R: Serial.println("R Type"); break;
+	case MAX31856_TCTYPE_S: Serial.println("S Type"); break;
+	case MAX31856_TCTYPE_T: Serial.println("T Type"); break;
+	case MAX31856_VMODE_G8: Serial.println("Voltage x8 Gain mode"); break;
+	case MAX31856_VMODE_G32: Serial.println("Voltage x8 Gain mode"); break;
+	default: Serial.println("Unknown"); break;
+	}
+
 
 	xTaskCreate(
 		taskLoop,          /* Task function. */
@@ -47,7 +71,24 @@ void setup()
 	Serial.println("Muh");
 }
 
-void loop() {}
+void loop() {
+	Serial.print("Cold Junction Temp: "); Serial.println(tempReader.readCJTemperature());
+
+	Serial.print("Thermocouple Temp: "); Serial.println(tempReader.readThermocoupleTemperature());
+	// Check and print any faults
+	uint8_t fault = tempReader.readFault();
+	if (fault) {
+		if (fault & MAX31856_FAULT_CJRANGE) Serial.println("Cold Junction Range Fault");
+		if (fault & MAX31856_FAULT_TCRANGE) Serial.println("Thermocouple Range Fault");
+		if (fault & MAX31856_FAULT_CJHIGH)  Serial.println("Cold Junction High Fault");
+		if (fault & MAX31856_FAULT_CJLOW)   Serial.println("Cold Junction Low Fault");
+		if (fault & MAX31856_FAULT_TCHIGH)  Serial.println("Thermocouple High Fault");
+		if (fault & MAX31856_FAULT_TCLOW)   Serial.println("Thermocouple Low Fault");
+		if (fault & MAX31856_FAULT_OVUV)    Serial.println("Over/Under Voltage Fault");
+		if (fault & MAX31856_FAULT_OPEN)    Serial.println("Thermocouple Open Fault");
+	}
+	delay(1000);
+}
 
 void taskLoop(void* params) {
 	Serial.println("Start render task");
